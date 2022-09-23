6f547912bf3aaed07250659d14d1976a867923d5
Nha Dang <nguoitacu@gmail.com>
2022-09-23 16:58:26 +0700

    fix: bug navication

--- src/layouts/VerticalLayout.tsx
+++ src/layouts/VerticalLayout.tsx
@@ -1,31 +1,29 @@
-import * as React from 'react'
 // ** React Imports
-import { useState } from 'react'
+import { useState } from 'react';
 
 // ** MUI Imports
-import { styled } from '@mui/material/styles'
-import Box, { BoxProps } from '@mui/material/Box'
+import Box, { BoxProps } from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
 
 // ** Icons Imports
 
 // ** Theme Config Import
-import themeConfig from 'src/configs/themeConfig'
+import themeConfig from 'src/configs/themeConfig';
 
 // ** Type Import
-import { LayoutProps } from 'src/layouts/types'
+import { LayoutProps } from 'src/layouts/types';
 
 // ** Components
-import DatePickerWrapper from 'src/styles/libs/react-datepicker'
-import Footer from 'src/components/shared-components/footer'
-import AppBar from './components/vertical/appBar'
-import Navigation from './components/vertical/navigation'
+import Footer from 'src/components/shared-components/footer';
+import DatePickerWrapper from 'src/styles/libs/react-datepicker';
+import AppBar from './components/vertical/appBar';
 
 // ** Styled Component
 
 const VerticalLayoutWrapper = styled('div')({
   height: '100%',
   display: 'flex',
-})
+});
 
 const MainContentWrapper = styled(Box)<BoxProps>({
   flexGrow: 1,
@@ -33,7 +31,7 @@ const MainContentWrapper = styled(Box)<BoxProps>({
   display: 'flex',
   minHeight: '100vh',
   flexDirection: 'column',
-})
+});
 
 const ContentWrapper = styled('main')(({ theme }) => ({
   flexGrow: 1,
@@ -44,65 +42,63 @@ const ContentWrapper = styled('main')(({ theme }) => ({
     paddingLeft: theme.spacing(4),
     paddingRight: theme.spacing(4),
   },
-}))
+}));
 
 const VerticalLayout = (props: LayoutProps) => {
   // ** Props
-  const { settings, children } = props
+  const { settings, children } = props;
 
   // ** Vars
-  const { contentWidth } = settings
-  const navWidth = themeConfig.navigationSize
+  const { contentWidth } = settings;
+  const navWidth = themeConfig.navigationSize;
 
   // ** States
-  const [navVisible, setNavVisible] = useState<boolean>(false)
+  const [navVisible, setNavVisible] = useState<boolean>(false);
 
   // ** Toggle Functions
-  const toggleNavVisibility = () => setNavVisible(!navVisible)
+  const toggleNavVisibility = () => setNavVisible(!navVisible);
 
   return (
-    <>
-      <VerticalLayoutWrapper className="layout-wrapper">
-        {/* Navigation Menu */}
-        <Navigation
-          navWidth={navWidth}
-          navVisible={navVisible}
-          setNavVisible={setNavVisible}
-          toggleNavVisibility={toggleNavVisibility}
+    <VerticalLayoutWrapper className="layout-wrapper">
+      {/* Navigation Menu */}
+      {/* <Navigation
+        navWidth={navWidth}
+        navVisible={navVisible}
+        setNavVisible={setNavVisible}
+        toggleNavVisibility={toggleNavVisibility}
+        {...props}
+      /> */}
+      <MainContentWrapper className="layout-content-wrapper">
+        {/* AppBar Component */}
+        <AppBar
+          //  toggleNavVisibility={toggleNavVisibility}
           {...props}
         />
-        <MainContentWrapper className="layout-content-wrapper">
-          {/* AppBar Component */}
-          <AppBar
-            //  toggleNavVisibility={toggleNavVisibility}
-            {...props}
-          />
 
-          {/* Content */}
-          <ContentWrapper
-            className="layout-page-content"
-            sx={{
-              ...(contentWidth === 'boxed' && {
-                mx: 'auto',
-                '@media (min-width:1440px)': { maxWidth: 1440 },
-                '@media (min-width:1200px)': { maxWidth: '100%' },
-              }),
-            }}
-          >
-            {children}
-          </ContentWrapper>
+        {/* Content */}
+        <ContentWrapper
+          className="layout-page-content"
+          sx={{
+            ...(contentWidth === 'boxed' && {
+              mx: 'auto',
+              '@media (min-width:1440px)': { maxWidth: 1440 },
+              '@media (min-width:1200px)': { maxWidth: '100%' },
+            }),
+          }}
+        >
+          {children}
+        </ContentWrapper>
 
-          {/* Footer Component */}
-          <Footer {...props} />
+        {/* Footer Component */}
+        <Footer {...props} />
 
-          {/* Portal for React Datepicker */}
-          <DatePickerWrapper sx={{ zIndex: 11 }}>
-            <Box id="react-datepicker-portal" />
-          </DatePickerWrapper>
-        </MainContentWrapper>
-      </VerticalLayoutWrapper>
-    </>
-  )
-}
+        {/* Portal for React Datepicker */}
+        <DatePickerWrapper sx={{ zIndex: 11 }}>
+          <Box id="react-datepicker-portal" />
+        </DatePickerWrapper>
+      </MainContentWrapper>
+    </VerticalLayoutWrapper>
+  );
+};
 
-export default VerticalLayout
+export default VerticalLayout;
--- src/navigation/vertical/index.tsx
+++ src/navigation/vertical/index.tsx
@@ -1,6 +1,4 @@
 // ** Icon imports
-import AccountCogOutline from 'mdi-material-ui/AccountCogOutline';
-import HomeOutline from 'mdi-material-ui/HomeOutline';
 
 // ** Type import
 import { VerticalNavItemsType } from 'src/layouts/types';
@@ -8,12 +6,10 @@ import { VerticalNavItemsType } from 'src/layouts/types';
 const navigation = (): VerticalNavItemsType => [
   {
     title: 'Dashboard',
-    icon: <HomeOutline />,
     path: '/',
   },
   {
     title: 'Account Settings',
-    icon: <AccountCogOutline />,
     path: '/User',
   },
 ];
